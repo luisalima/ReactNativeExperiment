3cd4da44e05ada51a094b957b6830e05f226f004
Rui Pereira <ruiafonsopereira@gmail.com>
2016-08-23 18:13:54 +0100

    Add left and right-side Drawers

--- index.ios.js
+++ index.ios.js
@@ -12,27 +12,28 @@ import {
 
 import TabBarContainer from './ios/TabBarContainer';
 
+import Drawer from 'react-native-drawer';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 class ReactNativeExperiment extends Component {
+  configureScene(route, routeStack) {
+    return Navigator.SceneConfigs.FloatFromRight
+  }
+
   renderScene(route, navigator) {
     const RouteComponent = route.component;
     return (
       <RouteComponent
         navigator={navigator}
         {...route.passProps}
-      />);
+      />
+    );
   }
 
-  configureScene(route, routeStack){
-    if (route.type == 'Modal') {
-      return Navigator.SceneConfigs.FloatFromBottom
-    }
-    return Navigator.SceneConfigs.FloatFromLeft
-  }
+  routeMapper() {
+    const that = this;
 
-  render() {
-    const NavigationBarRouteMapper={
+    return {
       LeftButton: function(route, navigator, index, navState) {
         if(index > 0) {
           return (
@@ -50,13 +51,32 @@ class ReactNativeExperiment extends Component {
                    style={styles.leftButtonIcon}
                  />
                 </View>
-            </TouchableHighlight>)
+            </TouchableHighlight>
+          )
         }
-        return null;
+
+        return (
+          <TouchableHighlight
+            style={styles.touchableHighlight}
+            underlayColor="transparent"
+            onPress={() => that._drawerLeft.open()}
+          >
+            <View
+              style={styles.iconContainer}
+            >
+              <Icon
+               name="bars"
+               size={20}
+               color="white"
+               style={styles.leftButtonIcon}
+             />
+            </View>
+          </TouchableHighlight>
+        );
       },
 
       RightButton: function(route, navigator, index, navState) {
-        if (route.onPressEllipsis || route.onPressRocket)
+        if (route.onPressEllipsis || route.onPressRocket) {
           return (
             <View
               style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}
@@ -81,41 +101,134 @@ class ReactNativeExperiment extends Component {
                     style={styles.rightButtonIcon}
                   />
                </TouchableHighlight>
-             </View>);
-        return null;
+             </View>
+           );
+         }
+
+         return (
+           <TouchableHighlight
+             style={styles.touchableHighlight}
+             underlayColor="transparent"
+             onPress={() => that._drawerRight.open()}
+           >
+             <View
+               style={styles.iconContainer}
+             >
+               <Icon
+                name="ellipsis-v"
+                size={20}
+                color="white"
+                style={styles.rightButtonIcon}
+              />
+             </View>
+           </TouchableHighlight>
+         );
       },
 
       Title: function(route, navigator, index, navState) {
         return (
-          <Text
-            style={styles.title}
-          >
+          <Text style={styles.title}>
             {route.title}
           </Text>
         );
       }
+    };
+  }
+
+  controlPanelLeft() {
+    return (
+      <View>
+        <Text>CONTROL PANEL LEFT!!!!</Text>
+      </View>
+    );
+  }
+
+  controlPanelRight() {
+    return (
+      <View>
+        <Text>CONTROL PANEL RIGHT!!!!</Text>
+      </View>
+    );
+  }
+
+  renderNavigatorWrapper() {
+    const MainView = () => {
+      return (
+        <TabBarContainer />
+      );
     };
 
     return (
       <Navigator
         configureScene={ this.configureScene }
         style={{flex: 1}}
-        initialRoute={{component: TabBarContainer, title: "Experiment List"}}
+        initialRoute={{component: MainView, title: "Experiment List"}}
         renderScene={ this.renderScene }
         navigationBar={
           <Navigator.NavigationBar
             style={styles.nav}
-            routeMapper={NavigationBarRouteMapper}
+            routeMapper={this.routeMapper()}
           />
         }
       />
     );
   }
+
+  render() {
+    return (
+      <Drawer
+        type="static"
+        openDrawerOffset={100}
+        closedDrawerOffset={-10}
+        styles={drawerLeftStyles}
+        tweenHandler={Drawer.tweenPresets.parallax}
+        ref={(ref) => this._drawerLeft = ref}
+        content={this.controlPanelLeft()}
+        panOpenMask={0.5}
+        side="left"
+        tapToClose
+      >
+        <Drawer
+          type="static"
+          openDrawerOffset={100}
+          closedDrawerOffset={-10}
+          styles={drawerRightStyles}
+          tweenHandler={Drawer.tweenPresets.parallax}
+          ref={(ref) => this._drawerRight = ref}
+          content={this.controlPanelRight()}
+          panOpenMask={0.5}
+          side="right"
+          tapToClose
+        >
+          {this.renderNavigatorWrapper()}
+        </Drawer>
+      </Drawer>
+    );
+  }
+}
+
+const drawerLeftStyles = {
+  drawer: { backgroundColor: 'green' },
+}
+
+const drawerRightStyles = {
+  drawer: { backgroundColor: 'red' },
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
   nav: {
     backgroundColor: '#BAA1A7'
+  },
+  navBtnText: {
+    fontSize: 15,
+    marginLeft: 15,
+    marginRight: 15
   },
   leftNavButtonText: {
     fontFamily: 'SnellRoundhand',
